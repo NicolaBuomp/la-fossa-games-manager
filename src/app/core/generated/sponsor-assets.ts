8e50521ae55debc5b51cd40a9444962428848f04
Nicola Buompane <nicolabuompane96@gmail.com>
2026-05-22 20:26:20 +0200

    feat: aggiungi nuovi sponsor e relative immagini nella gestione sponsor

--- src/app/core/generated/sponsor-assets.ts
+++ src/app/core/generated/sponsor-assets.ts
@@ -28,13 +28,33 @@ export const SPONSOR_ASSETS: SponsorAsset[] = [
     "category": "bronzo"
   },
   {
+    "name": "Gs Autofficina 2025 Page 0001",
+    "src": "/assets/sponsor/base/gs_autofficina_2025_page-0001.jpg",
+    "category": "bronzo"
+  },
+  {
     "name": "Lacasertana",
     "src": "/assets/sponsor/base/lacasertana.jpg",
     "category": "bronzo"
   },
   {
+    "name": "Onoranzefunebripalazzo",
+    "src": "/assets/sponsor/base/onoranzefunebripalazzo.jpeg",
+    "category": "bronzo"
+  },
+  {
     "name": "SALVATORE CEPPARULO",
     "src": "/assets/sponsor/base/SALVATORE_CEPPARULO_LOGO_BASE.png",
+    "category": "bronzo"
+  },
+  {
+    "name": "Tabacchin1",
+    "src": "/assets/sponsor/base/tabacchin1.jpeg",
+    "category": "bronzo"
+  },
+  {
+    "name": "Teofilo",
+    "src": "/assets/sponsor/base/teofilo.jpeg",
     "category": "bronzo"
   },
   {
